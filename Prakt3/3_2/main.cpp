a392976c09b2c90946ffea2771c070ec956aa4c9
RyaskovS <81479889+RyaskovS@users.noreply.github.com>
2021-06-20 13:19:58 +0300

    Add files via upload

---
+++ Prakt3/3_2/main.cpp
@@ -0,0 +1,59 @@
+#include <netinet/in.h>
+#include <unistd.h>
+#include <iostream>
+#include <arpa/inet.h>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+void Error(const char * why, const int exitCode=1)
+{
+    cerr<<why<<endl;
+    exit(exitCode);
+}
+int main()
+{
+
+    sockaddr_in * self_addr = new (sockaddr_in);
+    self_addr->sin_family = AF_INET;
+    self_addr->sin_port = 0;
+    self_addr->sin_addr.s_addr = 0;
+    char * buf= new char[256];
+    strcpy(buf,"Roflan\n");
+    int msgLen = strlen(buf);
+    sockaddr_in * srv_addr = new (sockaddr_in);
+    srv_addr->sin_family = AF_INET;
+    srv_addr->sin_port = htons(7);
+    srv_addr->sin_addr.s_addr = inet_addr("82.179.90.12");
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s==-1) {
+        Error("Error open socket",11);
+    }
+    int rc = bind(s,(const sockaddr*) self_addr,sizeof(sockaddr_in));
+    if (rc==-1) {
+        close(s);
+        Error("Error bind",12);
+    }
+    rc = connect(s,(const sockaddr*)srv_addr, sizeof(sockaddr_in));
+    if (rc == -1) {
+        close(s);
+        Error("Error connect",13);
+    }
+    rc=send(s,buf,msgLen,0);
+    if (rc == -1) {
+        close(s);
+        Error("Error send",14);
+    }
+    cout<<"We send "<<buf<<endl;
+    rc=recv(s,buf,256,0);
+    if (rc == -1) {
+        close(s);
+        Error("Error receive",15);
+    }
+    buf[rc]='\0';
+    cout<<"We receive "<<buf<<endl;
+    close(s);
+    delete self_addr;
+    delete srv_addr;
+    delete[] buf;
+    return 0;
+}
